a9ddb90be9400b49bece3284fcd6a8df4190dc81
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2024-10-21 00:52:43 +0200

    update

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -2,4 +2,4 @@
 user=$1
 docker build -t $user/arm-gnu-toolchain:11.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=11.3.1 --build-arg DOWNLOAD_URL_VERSION=11.3.rel1 . & \
 docker build -t $user/arm-gnu-toolchain:13.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=13.3.1 --build-arg DOWNLOAD_URL_VERSION=13.3.rel1 . & \
-docker build -t $user/arm-gnu-toolchain:12.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=12.3.1 --build-arg DOWNLOAD_URL_VERSION=12.3.rel1 
+docker build -t $user/arm-gnu-toolchain:12.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=12.3.1 --build-arg DOWNLOAD_URL_VERSION=12.3.rel1 .
