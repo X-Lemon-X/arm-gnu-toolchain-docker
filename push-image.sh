381d02d6f7cf69399c49de37a4e5949bd57bbc7a
Patryk Dudziński <dodpat02@gmail.com>
2025-02-05 16:56:44 +0100

    Update push-image.sh
    added  push to version 14.2

--- push-image.sh
+++ push-image.sh
@@ -1,6 +1,7 @@
 username=$1
-docker tag $username/arm-gnu-toolchain:13.3.1 $username/arm-gnu-toolchain:latest
+docker tag $username/arm-gnu-toolchain:14.2.1 $username/arm-gnu-toolchain:latest
 docker push $username/arm-gnu-toolchain:latest
+docker push $username/arm-gnu-toolchain:14.2.1
 docker push $username/arm-gnu-toolchain:13.3.1
 docker push $username/arm-gnu-toolchain:12.3.1
 docker push $username/arm-gnu-toolchain:11.3.1
