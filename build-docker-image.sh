bdcca28b66698a92839ef659f7cc343bbe8fc699
Patryk Dudziński <dodpat02@gmail.com>
2025-02-05 16:55:52 +0100

    Update build-docker-image.sh
    update 14.2

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -2,5 +2,5 @@
 user=$1
 docker build -t $user/arm-gnu-toolchain:11.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=11.3.1 --build-arg DOWNLOAD_URL_VERSION=11.3.rel1 . & \
 docker build -t $user/arm-gnu-toolchain:13.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=13.3.1 --build-arg DOWNLOAD_URL_VERSION=13.3.rel1 . & \
-docker build -t $user/arm-gnu-toolchain:13.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=14.2.1 --build-arg DOWNLOAD_URL_VERSION=14.2.rel1 . & \
+docker build -t $user/arm-gnu-toolchain:14.2.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=14.2.1 --build-arg DOWNLOAD_URL_VERSION=14.2.rel1 . & \
 docker build -t $user/arm-gnu-toolchain:12.3.1 --build-arg VERSION_ARM_GNU_TOOLCHAIN=12.3.1 --build-arg DOWNLOAD_URL_VERSION=12.3.rel1 .
